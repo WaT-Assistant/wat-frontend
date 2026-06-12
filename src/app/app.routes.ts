0b231e8ef6bbfac61286f42d05a72c7d33fdc708
NazarBondaruk <n07.bondaruk@gmail.com>
2026-06-12 15:15:10 +0300

    Feat: basic landing page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { MainLayout } from './layout/main-layout/main-layout';
+import { Landing } from './features/landing/landing';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainLayout,
+    children: [
+      {
+        path: '',
+        component: Landing
+      }
+    ]
+  }
+];
