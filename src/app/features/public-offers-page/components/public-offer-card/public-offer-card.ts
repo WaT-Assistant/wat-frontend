815b15c0138dc56840c1641840cfa46dbbd61489
NazarBondaruk <n07.bondaruk@gmail.com>
2026-06-22 19:05:43 +0300

    Feat: basic explore page structure and cards

--- src/app/features/public-offers-page/components/public-offer-card/public-offer-card.ts
+++ src/app/features/public-offers-page/components/public-offer-card/public-offer-card.ts
@@ -1,9 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { PublicOffer } from '../../public-offers-page';
+import { IconComponent } from '../../../../shared/components/icons/icon.component';
+import { DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-public-offer-card',
-  imports: [],
+  imports: [IconComponent, DecimalPipe],
   templateUrl: './public-offer-card.html',
   styleUrl: './public-offer-card.scss',
 })
