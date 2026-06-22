6882da897b958a7b3d8da82fd80e852208d98e87
NazarBondaruk <n07.bondaruk@gmail.com>
2026-06-22 19:20:38 +0300

    Fix: interceptor to identify expired jwt

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -1,10 +1,24 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { catchError, throwError } from 'rxjs';
 
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const router = inject(Router);
+
   const authReq = req.clone({
     withCredentials: true
   });
 
-  return next(authReq);
+  return next(authReq).pipe(
+    catchError((error: HttpErrorResponse) => {
+      if (error.status === 401) {
+        console.error('Expired token');
+        router.navigate(['/login']); 
+      }
+      
+      return throwError(() => error);
+    })
+  );;
 };
