16bcad3a67d2b286631a1627aca1cf9ebb0e423a
NazarBondaruk <n07.bondaruk@gmail.com>
2026-06-28 18:40:30 +0300

    Feat: badge for publixshed jo in dashboard

--- src/app/shared/components/icons/icon.component.ts
+++ src/app/shared/components/icons/icon.component.ts
@@ -31,6 +31,11 @@ import { Component, Input } from '@angular/core';
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.5v15m7.5-7.5h-15"></path>
         </svg>
       }
+      @case ('check') {
+        <svg [class]="customClass" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path>
+        </svg>
+      }
       @case ('pencil') {
         <svg class="w-4 h-4 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"></path>
