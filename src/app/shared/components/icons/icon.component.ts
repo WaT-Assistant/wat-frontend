621dfe122b0e523f3f6d4f4d5745f5bd65856068
NazarBondaruk <n07.bondaruk@gmail.com>
2026-07-06 15:12:43 +0300

    Refactor: auth pages svg

--- src/app/shared/components/icons/icon.component.ts
+++ src/app/shared/components/icons/icon.component.ts
@@ -85,6 +85,22 @@ import { Component, Input } from '@angular/core';
           <path stroke-linecap="round" stroke-linejoin="round" d="M3 3l18 18"/>
         </svg>
       }
+      @case ('hide-password') {
+        <svg [class]="customClass" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 
+          0 016 0z" />
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 
+          7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542
+          -7z" />
+        </svg>
+      
+      }
+      @case ('show-password') {
+        <svg [class]="customClass" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.542-7a10.05 10.05 0 011.52-3.37m3.96-3.96A9.96 9.96 0 0112 5c4.478 0 8.268 2.943 9.542 7a10.05 10.05 0 01-1.39 3.03M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3l18 18" />
+        </svg>
+      }
     }
   `
 })
